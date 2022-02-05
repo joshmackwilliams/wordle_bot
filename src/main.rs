6ae7c5593170782e6ee1f7e81e13762888287057
Joshua Williams <joshmackwilliams@protonmail.com>
2022-02-05 16:15:33 -0600

    Total rewrite, down to 3.47 average guesses

--- src/main.rs
+++ src/main.rs
@@ -16,153 +16,270 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
+use std::env;
 use std::fs::File;
-use std::io::{self, BufRead, stdin, Write};
-use rayon::iter::{ IntoParallelIterator, IntoParallelRefIterator, ParallelIterator };
+use std::io::{self, stdin, BufRead, Write};
 
 // The word length is hardcoded so that we can store buffers of the right length on the stack
 const WORD_LENGTH: usize = 5;
-const POSSIBLE_SIGNATURES: usize = 3_usize.pow(WORD_LENGTH as u32);
-
-const CHARACTERS_PER_LINE: usize = 80;
+const POSSIBLE_FEEDBACKS: usize = 3_usize.pow(WORD_LENGTH as u32);
 
 // Define these types for easy modification
-type Signature = u32;
-type Score = u32;
+type Feedback = u32;
+type Score = f32;
 
-fn get_signature(word: &str, guess: &str) -> Signature {
-    let word = word.as_bytes();
-    let guess = guess.as_bytes();
-    let mut used = [false; WORD_LENGTH];
-    let mut correct = [false; WORD_LENGTH];
-    let mut contained = [false; WORD_LENGTH];
-    // Correctly placed check
-    for i in 0..WORD_LENGTH {
-        if word[i] == guess[i] {
-            used[i] = true;
-            correct[i] = true;
+struct WordleBot {
+    n_words: usize,
+    n_solutions: usize,
+    solutions: Vec<String>,
+    all_words: Vec<String>,
+    feedbacks: Vec<Vec<Feedback>>,
+    remaining_set: Vec<usize>,
+    first_guess: usize,
+    is_first_guess: bool,
+    solutions_to_words: Vec<usize>,
+}
+
+impl WordleBot {
+    fn new(all_words: Vec<String>, solutions: Vec<String>) -> Self {
+        let n_words = all_words.len();
+        let n_solutions = solutions.len();
+        let mut bot = WordleBot {
+            n_words,
+            n_solutions,
+            all_words,
+            solutions,
+            feedbacks: Vec::new(),
+            remaining_set: (0..n_solutions).collect(),
+            first_guess: 0,
+            is_first_guess: false,
+            solutions_to_words: Vec::new(),
+        };
+        bot.calculate_solutions_to_words();
+        bot.calculate_feedbacks();
+        bot.calculate_first_guess();
+        bot
+    }
+
+    fn calculate_solutions_to_words(&mut self) {
+        self.solutions_to_words = self
+            .solutions
+            .iter()
+            .map(|solution_string| {
+                self.all_words
+                    .iter()
+                    .enumerate()
+                    .find(|(_, word_string)| *word_string == solution_string)
+                    .unwrap()
+                    .0
+            })
+            .collect();
+    }
+
+    fn solution_to_word(&self, solution: usize) -> usize {
+        self.solutions_to_words[solution]
+    }
+
+    fn calculate_first_guess(&mut self) {
+        self.first_guess = self.get_guess();
+        self.is_first_guess = true;
+    }
+
+    fn give_feedback(&mut self, word: usize, feedback: Feedback) {
+        self.is_first_guess = false;
+        let feedbacks = &self.feedbacks[word];
+        self.remaining_set.retain(|&x| feedbacks[x] == feedback);
+    }
+
+    fn get_guess(&self) -> usize {
+        if let Option::Some(solution) = self.get_solution() {
+            self.solution_to_word(solution)
+        } else if self.is_first_guess {
+            self.first_guess
+        } else {
+            (0..self.n_words)
+                .into_iter()
+                .map(|word| (word, self.score(word)))
+                .reduce(|x, y| if x.1 < y.1 {x} else {y})
+                .expect("Error: No guessable words!")
+                .0
         }
     }
-    // Incorrectly placed check
-    for i in 0..WORD_LENGTH {
-        if correct[i] {
-            continue;
+
+    fn score(&self, guess: usize) -> Score {
+        let feedbacks = &self.feedbacks[guess];
+        let mut class_sizes: [u32; POSSIBLE_FEEDBACKS] = [0; POSSIBLE_FEEDBACKS];
+        for &possible_solution in self.remaining_set.iter() {
+            class_sizes[feedbacks[possible_solution] as usize] += 1;
         }
-        for j in 0..WORD_LENGTH {
-            if used[j] {
+        class_sizes
+            .into_iter()
+            .take(POSSIBLE_FEEDBACKS - 1)
+            .map(|class_size| class_size as f32)
+            .map(|class_size| (class_size * (class_size + 1.0).log2()) as Score)
+            .sum()
+    }
+
+    fn calculate_feedbacks(&mut self) {
+        self.feedbacks = (0..self.n_words)
+            .into_iter()
+            .map(|guess| {
+                (0..self.n_solutions)
+                    .map(|solution| self.calculate_feedback(solution, guess))
+                    .collect()
+            })
+            .collect();
+    }
+
+    fn calculate_feedback(&self, word: usize, guess: usize) -> Feedback {
+        let word = self.solutions[word].as_bytes();
+        let guess = self.all_words[guess].as_bytes();
+        let mut used = [false; WORD_LENGTH];
+        let mut correct = [false; WORD_LENGTH];
+        let mut contained = [false; WORD_LENGTH];
+        // Correctly placed check
+        for i in 0..WORD_LENGTH {
+            if word[i] == guess[i] {
+                used[i] = true;
+                correct[i] = true;
+            }
+        }
+        // Incorrectly placed check
+        for i in 0..WORD_LENGTH {
+            if correct[i] {
                 continue;
             }
-            if guess[i] == word[j] {
-                contained[i] = true;
-                used[j] = true;
-                break;
+            for j in 0..WORD_LENGTH {
+                if used[j] {
+                    continue;
+                }
+                if guess[i] == word[j] {
+                    contained[i] = true;
+                    used[j] = true;
+                    break;
+                }
             }
         }
+        // Convert these arrays to a numerical feedback
+        let mut feedback = 0;
+        for i in 0..WORD_LENGTH {
+            feedback *= 3;
+            if correct[i] {
+                feedback += 2;
+            } else if contained[i] {
+                feedback += 1;
+            }
+        }
+        feedback
     }
-    // Convert these arrays to a numerical signature
-    let mut signature = 0;
-    for i in 0..WORD_LENGTH {
-        signature *= 3;
-        if correct[i] {
-            signature += 2;
-        } else if contained[i] {
-            signature += 1;
+
+    fn get_feedback(&self, solution: usize, guess: usize) -> Feedback {
+        self.feedbacks[guess][solution]
+    }
+
+    fn get_solution(&self) -> Option<usize> {
+        if self.remaining_set.len() == 1 {
+            Option::Some(self.remaining_set[0])
+        } else {
+            Option::None
         }
     }
-    signature
+
+    fn solution_string(&self, word: usize) -> &String {
+        &self.solutions[word]
+    }
+
+    fn word_string(&self, word: usize) -> &String {
+        &self.all_words[word]
+    }
+
+    fn reset(&mut self) {
+        self.is_first_guess = true;
+        self.remaining_set = (0..self.n_solutions).collect();
+    }
 }
 
-fn to_signature(input: &str) -> Signature {
+fn feedback_from_string(input: &str) -> Feedback {
     let input = input.as_bytes();
-    let mut signature = 0;
-    for i in 0..WORD_LENGTH {
-        signature *= 3;
-        if input[i] == b'g' {
-            signature += 2;
-        } else if input[i] == b'y' {
-            signature += 1;
+    let mut feedback = 0;
+    for &c in input.iter() {
+        feedback *= 3;
+        if c == b'g' {
+            feedback += 2;
+        } else if c == b'y' {
+            feedback += 1;
         }
     }
-    signature
+    feedback
 }
 
-fn score(guess: &str, possible_solutions: &[String]) -> Score {
-    let mut group_sizes: [Score; POSSIBLE_SIGNATURES] = [0; POSSIBLE_SIGNATURES];
-    for word in possible_solutions {
-        let signature = get_signature(word, guess);
-        group_sizes[signature as usize] += 1;
-    }
-    let mut score = 0;
-    // We don't count the last signature, which is "ggggg"
-    for i in 0..group_sizes.len() - 1 {
-        let group_size = group_sizes[i];
-        score += group_size * group_size;
-    }
-    score
-}
-
-// Prints a vector of words in columns
-fn print_words(words: &[String]) {
-    for i in 0..words.len() {
-        print!("{} ", words[i]);
-        if (i + 1) % (CHARACTERS_PER_LINE / 6) == 0 {
-            println!("");
+fn display_average_guesses(mut bot: WordleBot) {
+    let mut total_guesses = 0;
+    for solution in 0..bot.n_solutions {
+        bot.reset();
+        let solution_as_word = bot.solution_to_word(solution);
+        loop {
+            let guess: usize = bot.get_guess();
+            total_guesses += 1;
+            if guess == solution_as_word {
+                break;
+            }
+            bot.give_feedback(guess, bot.get_feedback(solution, guess));
         }
     }
-    println!("");
+    let average_guesses = (total_guesses as f64) / (bot.n_solutions as f64);
+    println!("Average guesses used: {average_guesses}");
+}
+
+fn play_game(mut bot: WordleBot) {
+    loop {
+        match bot.get_solution() {
+            Option::Some(solution) => {
+                let solution_string = bot.solution_string(solution);
+                println!("Solution found: {solution_string}");
+                break;
+            }
+            Option::None => (),
+        }
+        let guess = bot.get_guess();
+        let guess_string = bot.word_string(guess);
+
+        println!("My guess is {guess_string}",);
+        print!("Please enter feedback using 'g', 'y', and 'x': ");
+        io::stdout().flush().unwrap();
+        let mut feedback = String::new();
+        stdin().read_line(&mut feedback).unwrap();
+        println!();
+        let feedback = feedback.trim();
+        let feedback = feedback_from_string(feedback);
+
+        bot.give_feedback(guess, feedback);
+    }
 }
 
 fn main() {
-    let mut possible_solutions: Vec<String> =
-        io::BufReader::new(File::open("dictionary_solutions.txt").unwrap())
-            .lines()
-            .map(|x| x.unwrap())
-            .collect();
-    let guessable_words: Vec<String> =
-        io::BufReader::new(File::open("dictionary_full.txt").unwrap())
-            .lines()
-            .map(|x| x.unwrap())
-            .collect();
-    println!(
-        "{} guessable words, {} possible solutions",
-        guessable_words.len(),
-        possible_solutions.len()
-    );
-    loop {
-        // Guess
-        let guess = guessable_words
-            .par_iter()
-            .map(|word| (word, score(word, &possible_solutions)))
-            .min_by(|x, y| x.1.cmp(&y.1))
-            .expect("Error: No guessable words!");
-        println!("My guess is {}, with a score of {}", guess.0, guess.1);
+    let mut args = env::args();
+    let _path_to_executable = args.next();
+    let mode = args.next().unwrap_or_else(|| "game".to_string());
+    let solutions_filename = args
+        .next()
+        .unwrap_or_else(|| "dictionary_solutions.txt".to_string());
+    let dictionary_filename = args
+        .next()
+        .unwrap_or_else(|| "dictionary_full.txt".to_string());
 
-        // Get feedback from user
-        print!("Please enter feedback using 'g', 'y', and 'x': ");
-        io::stdout().flush().unwrap();
-        let mut signature = String::new();
-        stdin().read_line(&mut signature).unwrap();
-        println!("");
-        let signature = signature.trim();
+    let solutions: Vec<String> = io::BufReader::new(File::open(solutions_filename).unwrap())
+        .lines()
+        .map(|x| x.unwrap())
+        .collect();
+    let all_words: Vec<String> = io::BufReader::new(File::open(dictionary_filename).unwrap())
+        .lines()
+        .map(|x| x.unwrap())
+        .collect();
+    let bot = WordleBot::new(all_words, solutions);
 
-        // Trim down solutions based on feedback
-        let signature = to_signature(signature);
-        possible_solutions = possible_solutions
-            .into_par_iter()
-            .filter(|word| -> bool { signature == get_signature(word, guess.0) })
-            .collect();
-
-        // Check for end conditions and print information
-        if possible_solutions.len() == 1 {
-            println!("Word found: {}", possible_solutions[0]);
-            break;
-        } else if possible_solutions.is_empty() {
-            println!("Error: No remaining possible words!");
-            break;
-        } else {
-            println!("There are {} remaining possible solutions: ", possible_solutions.len());
-            print_words(&possible_solutions);
-            println!("");
-        }
+    match mode.as_str() {
+        "average" => display_average_guesses(bot),
+        _ => play_game(bot),
     }
 }
